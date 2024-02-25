50a891de2c53a5fabf66f15fb624248276ce3468
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-02-24 23:39:18 -0300

    updating js and removal image  backgroung

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const form = document.getElementById('form-contact');
-form.addEventListener('submit', function(e) {
+let rows = '';
+    
+    form.addEventListener('submit', function(e) {
     e.preventDefault();
+    
+    addRow ();
+    updateTable ();
 
+});
+
+function addRow() {
     const inputContactName = document.getElementById ('contact-name');
     const inputPhoneNumber = document.getElementById ('phone-number');
 
-    let row = '';
-    row += `${inputContactName.value}`;
-    row += `${inputPhoneNumber.value}`;
-    row += '';
+    let row = '<tr>';
+    row += `<td>${inputContactName.value}</td>`;
+    row += `<td>${inputPhoneNumber.value}</td>`;
+    row += '<tr>';
 
+    rows += row;
+
+    inputContactName.value = '';
+    inputPhoneNumber.value = '';
+};
+
+function updateTable() {
     const tablebody = document.querySelector('tbody');
-    
-    tablebody.innerHTML = row;
-});
+    tablebody.innerHTML = rows;
+};
