bbe996557ca7847ec6126bf5afbcae4217c174f8
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-02-27 22:44:52 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#header-button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#cancel-button').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+    })
+})
