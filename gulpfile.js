d481bc0d11d1768400b719ebe38ab4956fcdbf5d
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-04-29 22:32:44 -0300

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = function(done) {
-    styles(); 
-    done(); // Indica que a tarefa está completa
-};
+function watch() {
+    gulp.watch('./src/styles/*.scss', styles); // Observa mudanças nos arquivos SCSS e executa a tarefa 'styles'
+}
+
+exports.styles = styles; // Exporta a tarefa 'styles'
+exports.watch = watch; // Exporta a tarefa 'watch'
+exports.default = gulp.series(styles); // Define a tarefa padrão
