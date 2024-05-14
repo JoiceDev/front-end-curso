12e5f43f46717f9cede12747be735559773f9ae1
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-05-14 01:12:09 -0300

    first commit

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    return "Olá," + nome + "!";
+}
+console.log(multiplicacao(2, 3));
+console.log(saudacao("João"));
